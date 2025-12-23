503aed3d48148116c5bce28433ecf01f8d5f35fb
pahi-dev <pahitharan2@gmail.com>
2025-12-23 21:39:46 +0530

    added employee GET API for pagination

--- src/main/java/com/example/Practice/controller/EmployeeController.java
+++ src/main/java/com/example/Practice/controller/EmployeeController.java
@@ -4,7 +4,10 @@ import com.example.Practice.entity.Employee;
 import com.example.Practice.service.EmployeeService;
 import com.example.Practice.utills.EmployeeNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class EmployeeController {
@@ -26,6 +29,15 @@ public class EmployeeController {
     public List<Employee> getAllEmployees() {
         return employeeService.getAllEmployees();
     }
+
+    @GetMapping("/api/v1/employees/pagination")
+    public Page<Employee> getEmployeesWithPagination(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "5") int size
+    ) {
+        return employeeService.getEmployeesWithPagination(page, size);
+    }
+
 
     @DeleteMapping("/api/v1/{id}")
     private Employee deleteEmployee(@PathVariable Long id) throws EmployeeNotFoundException {
